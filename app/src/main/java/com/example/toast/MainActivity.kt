8e3fb1e473e0567015d2b0d7a09485e8e90f59e3
Kamkoum Sabrine <kamkoumsabrine30@gmail.com>
2022-10-14 20:08:26 +0200

    first commit

---
+++ app/src/main/java/com/example/toast/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.toast
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    lateinit var button : Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button = findViewById(R.id.btn)
+
+        button.setOnClickListener{
+            Toast.makeText(applicationContext, "this is a toast", Toast.LENGTH_LONG).show()
+        }
+    }
+}
